6c3d51c6dccf3d046689bcd96e38dcebf4b6f45f
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-09-20 18:16:36 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-import FirstPage from './pages/FirstPage/FirstPage';
-import SecondPage from './pages/SecondPage/SecondPage';
-import HalfPage from './pages/HalfPage/HalfPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import { AppWrapper } from './App.styled';
 import { GlobalStyle } from './components/GlobalStyles/GlobalStyles.styled';
@@ -16,11 +13,6 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
-
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
