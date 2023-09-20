b3317f75c477b2f8e1ca5986fcbc80a7bc9f8115
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-09-20 13:36:20 +0300

    commit

---
+++ src/pages/WelcomePages/SignInPage/SignInPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SignInPageStyled } from './SignInPage.styled';
+
+const SignInPage = () => {
+  return <SignInPageStyled>SignInPage</SignInPageStyled>;
+};
+
+export default SignInPage;
---
+++ src/pages/WelcomePages/StartPage/StartPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StartPageStyled } from './StartPage.styled';
+
+const StartPage = () => {
+  return <StartPageStyled>StartPage</StartPageStyled>;
+};
+
+export default StartPage;
