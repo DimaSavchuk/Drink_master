d647124ccf15b96f476f9aec0efd209f3d7418fd
Inna-Melnyk <inamelnyk@ukr.net>
2023-09-26 17:18:40 +0200

    registration/login

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,40 @@
-export const selectUser = (state) => state.auth.user;
-export const selectUserArray = (state) => state.userInfo.user;
+// export const selectUser = (state) => state.auth.user;
+// export const selectUserArray = (state) => state.userInfo.user;
+
+import { configureStore } from '@reduxjs/toolkit';
+import { authReducer } from './auth/authSlice';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// // Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    // user: userReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
