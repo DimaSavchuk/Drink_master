a8ad1cf0129c82db2c4fce973b35036343a0bee7
Liza <lizapikovskaya@gmail.com>
2023-09-26 17:03:00 +0300

    fix bcg

--- src/components/MobileMenu/MobileMenu.styled.jsx
+++ src/components/MobileMenu/MobileMenu.styled.jsx
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 import ellipseMobileBlue from '../../assets/mobile-menu/ellipse-mobile-blue.png';
 import ellipseMobileYellow from '../../assets/mobile-menu/ellipse-mobile-yellow.png';
 import ellipseMobileRightBottom from '../../assets/mobile-menu/ellipseright-bottom-mobile.png';
+
+import ellipseTabletBlue from '../../assets/mobile-menu/ellipse-blue-tablet.png';
+import ellipseTabletYellow from '../../assets/mobile-menu/ellipse-yellow-tablet.png';
+
 export const MenuWrapper = styled.div`
   position: fixed;
   width: 100vw;
@@ -22,10 +26,84 @@ export const MenuWrapper = styled.div`
   background-position:
     110% 90%,
     60% -130px,
-    -15% -100%;
+    -15% -170%;
   background-size: auto;
   background-repeat: no-repeat;
+  @media (min-width: 475px) {
+    background-image: url(${ellipseMobileBlue}), url(${ellipseMobileYellow}),
+      url(${ellipseMobileRightBottom});
+    background-position:
+      140% 90%,
+      70% -130px,
+      -40% -170%;
+  }
+  @media (min-width: 575px) {
+    background-image: url(${ellipseMobileBlue}), url(${ellipseMobileYellow}),
+      url(${ellipseMobileRightBottom});
+    background-position:
+      170% 90%,
+      120% -130px,
+      -80% -170%;
+  }
+  @media (min-width: 675px) {
+    background-image: url(${ellipseMobileBlue}), url(${ellipseMobileYellow}),
+      url(${ellipseMobileRightBottom});
+    background-position:
+      220% 90%,
+      160% -130px,
+      -120% -170%;
+  }
+  @media (min-width: 720px) {
+    background-image: url(${ellipseMobileBlue}), url(${ellipseMobileYellow}),
+      url(${ellipseMobileRightBottom});
+    background-position:
+      270% 90%,
+      -300% -130px,
+      -160% -170%;
+  }
   @media (min-width: 768px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -400px -65px,
+      450px 300px;
+  }
+  @media (min-width: 900px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -450px -65px,
+      500px 300px;
+  }
+  @media (min-width: 1000px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -450px -65px,
+      600px 300px;
+  }
+  @media (min-width: 1100px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -450px -65px,
+      700px 300px;
+  }
+  @media (min-width: 1200px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -450px -65px,
+      800px 300px;
+  }
+  @media (min-width: 1300px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -450px -65px,
+      900px 300px;
+  }
+  @media (min-width: 1400px) {
+    background-image: url(${ellipseTabletYellow}), url(${ellipseTabletBlue});
+    background-position:
+      -450px -65px,
+      1000px 300px;
+  }
+  @media (min-width: 1440px) {
     display: none;
   }
 `;
@@ -34,6 +112,9 @@ export const MobileContainer = styled.div`
   padding: 232px 20px 200px;
   height: 100%;
   overflow: auto;
+  @media (min-width: 768px) {
+    padding: 340px 20px 200px;
+  }
 `;
 
 export const List = styled.ul`
@@ -43,7 +124,6 @@ export const List = styled.ul`
   gap: 16px;
 
   @media (min-width: 768px) {
-    display: none;
   }
 `;
 
