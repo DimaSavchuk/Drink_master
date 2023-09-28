22e38680013ef8d0340cb214917102ddf682334a
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-09-29 00:47:51 +0300

    search input

--- src/components/AddDrinkSelectMenu/AddDrinkSelectMenu.jsx
+++ src/components/AddDrinkSelectMenu/AddDrinkSelectMenu.jsx
@@ -3,6 +3,7 @@ import {
   CustomSelect,
   DropMenu,
   Label,
+  SearchInput,
   SelectItem,
   SelectWrapper,
 } from './AddDrinkSelectMenu.styled';
@@ -14,7 +15,13 @@ const AddDrinkDropdownMenu = ({ items, title, error }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState('');
 
+  const [searchQuery, setSearchQuery] = useState('');
+
   const titleValue = title.toLowerCase();
+  const filteredItems = () =>
+    items.filter((item) =>
+      item.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
 
   const [, , { setValue }] = useField({ name: titleValue });
 
@@ -26,6 +33,7 @@ const AddDrinkDropdownMenu = ({ items, title, error }) => {
     setSelectedValue(item);
     toggleMenu();
     setValue(item);
+    setSearchQuery('');
   };
 
   return (
@@ -40,13 +48,31 @@ const AddDrinkDropdownMenu = ({ items, title, error }) => {
         )}
       </CustomSelect>
       {isOpen && items && (
-        <DropMenu>
-          {items.map((item, index) => (
-            <SelectItem key={index} onClick={() => handleClickItem(item)}>
-              {item}
-            </SelectItem>
-          ))}
-        </DropMenu>
+        <>
+          <DropMenu
+            onFocus={() => {
+              console.log('focus');
+            }}
+            onBlur={() => setIsOpen(false)}
+          >
+            {items.length > 20 && (
+              <SearchInput
+                type="text"
+                name={`${title}Filter`}
+                placeholder="Search..."
+                onChange={(e) => {
+                  setSearchQuery(e.target.value);
+                  console.log(searchQuery);
+                }}
+              />
+            )}
+            {filteredItems().map((item, index) => (
+              <SelectItem key={index} onClick={() => handleClickItem(item)}>
+                {item}
+              </SelectItem>
+            ))}
+          </DropMenu>
+        </>
       )}
       {error ? <ErrorText>{error}</ErrorText> : null}
     </SelectWrapper>
