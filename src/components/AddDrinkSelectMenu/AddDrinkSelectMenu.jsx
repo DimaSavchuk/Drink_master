39d2cd1b23790c2f7116a273b47584472fd604e3
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-09-29 18:29:20 +0300

    update toggle menu with Refs

--- src/components/AddDrinkSelectMenu/AddDrinkSelectMenu.jsx
+++ src/components/AddDrinkSelectMenu/AddDrinkSelectMenu.jsx
@@ -10,18 +10,20 @@ import {
 import { SelectOpenArrow } from '../SelectOpenArrow/SelectOpenArrow';
 import { useField } from 'formik';
 import { ErrorText } from '../AddDrinkTitle/AddDrinkTitle.styled';
+import { useCallback } from 'react';
 
 const AddDrinkDropdownMenu = ({ items, title, error }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState('');
-
   const [searchQuery, setSearchQuery] = useState('');
 
+  const selectRef = useRef();
+  const searchInputRef = useRef();
+  const dropdownMenuRef = useRef();
+
   const titleValue = title.toLowerCase();
-  const filteredItems = () =>
-    items.filter((item) =>
-      item.toLowerCase().includes(searchQuery.toLowerCase()),
-    );
+  const filteredItems = (value) =>
+    items.filter((item) => item.toLowerCase().includes(value.toLowerCase()));
 
   const [, , { setValue }] = useField({ name: titleValue });
 
@@ -36,37 +38,58 @@ const AddDrinkDropdownMenu = ({ items, title, error }) => {
     setSearchQuery('');
   };
 
+  useEffect(() => {
+    if (searchInputRef) {
+      setSearchQuery('');
+      if (isOpen && searchInputRef.current) {
+        searchInputRef.current.focus();
+      }
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (selectRef.current && selectRef.current.contains(e.target)) {
+        setIsOpen(true);
+      }
+      if (
+        dropdownMenuRef.current &&
+        !dropdownMenuRef.current.contains(e.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('click', handler);
+    return () => {
+      window.removeEventListener('click', handler);
+    };
+  });
+
   return (
     <SelectWrapper>
-      <CustomSelect type="button" onClick={toggleMenu}>
+      <CustomSelect type="button" ref={selectRef}>
         <Label>{title}</Label>
         {items && (
           <div style={{ display: 'flex' }}>
-            <span>{selectedValue ? selectedValue : items[0]}</span>
+            <span>{selectedValue ? selectedValue : ''}</span>
             <SelectOpenArrow isOpen={isOpen} />
           </div>
         )}
       </CustomSelect>
       {isOpen && items && (
         <>
-          <DropMenu
-            onFocus={() => {
-              console.log('focus');
-            }}
-            onBlur={() => setIsOpen(false)}
-          >
+          <DropMenu ref={dropdownMenuRef}>
             {items.length > 20 && (
               <SearchInput
+                ref={searchInputRef}
                 type="text"
                 name={`${title}Filter`}
                 placeholder="Search..."
-                onChange={(e) => {
-                  setSearchQuery(e.target.value);
-                  console.log(searchQuery);
-                }}
+                onChange={(e) => setSearchQuery(e.target.value)}
               />
             )}
-            {filteredItems().map((item, index) => (
+            {filteredItems(searchQuery).map((item, index) => (
               <SelectItem key={index} onClick={() => handleClickItem(item)}>
                 {item}
               </SelectItem>
