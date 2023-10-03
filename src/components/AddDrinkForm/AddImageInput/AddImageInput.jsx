e4fded8dac84cc03ae997a9738e8b7e0619c6332
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-10-03 13:33:48 +0300

    fix

--- src/components/AddDrinkForm/AddImageInput/AddImageInput.jsx
+++ src/components/AddDrinkForm/AddImageInput/AddImageInput.jsx
@@ -26,16 +26,6 @@ const AddImageInput = ({ setValue, fileInputRef }) => {
     }
   };
 
-
-  useEffect(() => {
-  
-    if (field.value === null) {
-      setFileUrl('');
-      return;
-    }
-  }, [field]);
-
-
   return (
     <InputWrapper>
       <input
